d3b56c708010eb1bee2082a543ebfae4e054d578
Lewin671 <2596736318@qq.com>
2020-10-31 15:28:43 +0800

    add install certificate script

---
+++ install_certificate.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#fonts color
+Green="\033[32m"
+Red="\033[31m"
+# Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+
+#notification information
+# Info="${Green}[信息]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[错误]${Font}"
+
+
+amce_sh_file="/root/.acme.sh/acme.sh"
+ssl_update_file="/usr/bin/ssl_update.sh"
+
+# 获取Linux version
+source '/etc/os-release'
+
+check_system() {
+    if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]]; then
+        echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font}"
+        INS="yum"
+    elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]]; then
+        echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font}"
+        INS="apt"
+        $INS update
+        ## 添加 Nginx apt源
+    elif [[ "${ID}" == "ubuntu" && $(echo "${VERSION_ID}" | cut -d '.' -f1) -ge 16 ]]; then
+        echo -e "${OK} ${GreenBG} 当前系统为 Ubuntu ${VERSION_ID} ${UBUNTU_CODENAME} ${Font}"
+        INS="apt"
+        $INS update
+    else
+        echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font}"
+        exit 1
+    fi
+
+    $INS install dbus
+
+    systemctl stop firewalld
+    systemctl disable firewalld
+    echo -e "${OK} ${GreenBG} firewalld 已关闭 ${Font}"
+
+    systemctl stop ufw
+    systemctl disable ufw
+    echo -e "${OK} ${GreenBG} ufw 已关闭 ${Font}"
+}
+
+
+judge() {
+    if [[ 0 -eq $? ]]; then
+        echo -e "${OK} ${GreenBG} $1 完成 ${Font}"
+        sleep 1
+    else
+        echo -e "${Error} ${RedBG} $1 失败${Font}"
+        exit 1
+    fi
+}
+
+ssl_install() {
+    if [[ "${ID}" == "centos" ]]; then
+        ${INS} install socat nc -y
+    else
+        ${INS} install socat netcat -y
+    fi
+    judge "安装 SSL 证书生成脚本依赖"
+
+    curl https://get.acme.sh | sh
+    judge "安装 SSL 证书生成脚本"
+}
+
+acme() {
+    flags="--standalone -k ec-256 --force"
+    if "$HOME"/.acme.sh/acme.sh --issue -d "${domain}" ${flags} --test; then
+        echo -e "${OK} ${GreenBG} SSL 证书测试签发成功，开始正式签发 ${Font}"
+        rm -rf "$HOME/.acme.sh/${domain}_ecc"
+        sleep 2
+    else
+        echo -e "${Error} ${RedBG} SSL 证书测试签发失败 ${Font}"
+        rm -rf "$HOME/.acme.sh/${domain}_ecc"
+        exit 1
+    fi
+
+    if "$HOME"/.acme.sh/acme.sh --issue -d "${domain}" ${flags}; then
+        echo -e "${OK} ${GreenBG} SSL 证书生成成功 ${Font}"
+        sleep 2
+        mkdir /data
+        if "$HOME"/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /data/${domain}.crt --keypath /data/${domain}.key --ecc --force; then
+            echo -e "${OK} ${GreenBG} 证书配置成功 ${Font}"
+            sleep 2
+        fi
+        echo "证书路径在/data文件夹"
+    else
+        echo -e "${Error} ${RedBG} SSL 证书生成失败 ${Font}"
+        rm -rf "$HOME/.acme.sh/${domain}_ecc"
+        exit 1
+    fi
+
+    # 如果证书快要过期了，脚本会自动更新证书
+    # 脚本自动更新，可以使用以下命令
+    "$HOME"/.acme.sh/acme.sh --upgrade --auto-upgrade
+}
+
+
+read -rp "请输入你的域名信息(eg:www.qingyingliu.com):" domain
+check_system
+ssl_install
+acme
